8362bcab0a4e924994039b39a52172a2453245be
Ben <murobocup42@ubuntu>
2013-08-21 14:33:56 +1000

    Added pin definition header. pin_def.xls has address for use in I2C (note MPU9150 has address hardcoded into library) sketch has libraries added and some variables from skpang example (adding them slowly)

---
+++ software/arduino/sketch/pin_defs.h
@@ -0,0 +1,24 @@
+// Pin definitions - mirrored in ../../hardware/pin_defs.xls
+
+#ifndef PIN_DEF_H
+
+#define PIN_DEF_H 
+
+/* Define Joystick connection */
+#define UP     A1
+#define RIGHT  A2
+#define DOWN   A3
+#define CLICK  A4
+#define LEFT   A5
+
+// SPI pins
+#define CAN_CS  10
+#define SD_CS   9
+//#define IMU_CS   53
+
+// Analog inputs
+#define MAST  A5
+#define REACH A6
+#define LOAD  A7
+
+#endif
--- software/arduino/sketch/sketch.ino
+++ software/arduino/sketch/sketch.ino
@@ -1,4 +1,26 @@
+
+
+#include <SPI.h>
+
+// Updated SD library
+#include <SD.h>
+// From skpang http://www.skpang.co.uk/catalog/arduino-canbus-shield-with-usd-card-holder-p-706.html
+#include <Canbus.h>
+
+#include "pin_defs.h"
+
+// Set the baud rate
+can_speed = CANSPEED_250;
+
+// Variables for SD card logging
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+SdFile file;
+
+
 void setup() {
+
 }
 
 void loop() {
