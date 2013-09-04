3f9a3768bf7d6310635c25c9c5b2830f743787be
Ben <murobocup42@ubuntu>
2013-09-04 14:41:59 +1000

    Added conditional compilation statements to allow switching on and off functionaility (not complete)

--- software/arduino/integrated_test/integrated_test.ino
+++ software/arduino/integrated_test/integrated_test.ino
@@ -1,94 +1,163 @@
-#include <Wire.h>
-#include <Rtc_Pcf8563.h>
+//=== Configuration ==========================================================//
+// Comment out unecessary/unconnected components
+// #defines used to remove components from compilation (should happen) rather 
+// than a check which will leave code in place leading to a bloated file
 
-#include <SD.h>
+#define DEBUG       // Open up USB serial connection and other debug info
+#define MEGA1280    // Broken SPI pins so turn off CAN and SD
 
-#include <SoftwareSerial.h>
+#define CAN_ON      // Using the CAN bus
+#define LCD_ON      // Using the serial LCD
+#define IMU_ON      // Using the IMU/MPU
+#define SD_ON       // Logging to SD
+#define RTC_ON      // Using real time clock
+#define JOYSTICK_ON // Using joystick. Turn off if using a UNO
 
-#include "I2Cdev.h"
-#include "MPU9150Lib.h"
-#include "CalLib.h"
-#include <dmpKey.h>
-#include <dmpmap.h>
-#include <inv_mpu.h>
-#include <inv_mpu_dmp_motion_driver.h>
-#include <EEPROM.h>
+//============================================================================//
 
-//=== RTC ===//
-//init the real time clock
-Rtc_Pcf8563 rtc;
+//=== Libraries ==============================================================//
+// Include libraries of components being used.
 
-char *cur_time, *prev_time;
+#ifndef MEGA1280
+    #ifdef CAN_ON
+        #include <SPI.h>
+    #endif
+    
+    #ifdef SD_ON
+        #include <SPI.h>
+        #include <SD.h>
+    #endif
+#endif
 
-//=== SD ===//
-const int chipSelect = 9;
+#ifdef LCD_ON
+    #include <SoftwareSerial.h>
+#endif
+    
+#ifdef IMU_ON
+    #include <Wire.h>
+    #include "I2Cdev.h"
+    #include "MPU9150Lib.h"
+    #include "CalLib.h"
+    #include <dmpKey.h>
+    #include <dmpmap.h>
+    #include <inv_mpu.h>
+    #include <inv_mpu_dmp_motion_driver.h>
+    #include <EEPROM.h>
+#endif
 
-File dataFile;
+#ifdef RTC_ON
+    #include <Wire.h>
+    #include <Rtc_Pcf8563.h>
+#endif
+//============================================================================//
 
-//=== LCD ===//
-SoftwareSerial sLCD =  SoftwareSerial(3, 6);
+//=== Init ===================================================================//
+// Initialise vairables for each component
 
-#define COMMAND 0xFE
-#define CLEAR   0x01
-#define LINE0   0x80
-#define LINE1   0xC0
+// On board LED
+int LED1 = 13;
 
-
-/* Define Joystick connection */
-#define UP     A1
-#define RIGHT  A2
-#define DOWN   A3
-#define CLICK  A4
-#define LEFT   A5
-
-int LED2 = 8;
-int LED3 = 7;
-
-void clear_lcd(void);
-
+// Timing count
 int count = 0;
 
-//===MPU===//
-//  DEVICE_TO_USE selects whether the IMU at address 0x68 (default) or 0x69 is used
-//    0 = use the device at 0x68
-//    1 = use the device at ox69
+#ifdef MEGA1280
+    //=== CAN ===//
+    #ifdef CAN_ON
+        // CAN LEDs
+        int LED2 = 8;
+        int LED3 = 7;
+    #endif
+    
+    //=== SD ===//
+    #ifdef SD_ON
+        const int chipSelect = 9;
 
-#define  DEVICE_TO_USE    0
+        File dataFile;
+    #endif
+#endif
 
-MPU9150Lib MPU;                                              // the MPU object
+//=== RTC ===//
+#ifdef RTC_ON
+    //init the real time clock
+    Rtc_Pcf8563 rtc;
 
-//  MPU_UPDATE_RATE defines the rate (in Hz) at which the MPU updates the sensor data and DMP output
+    // Keep track of time, used to allow track time between seconds
+    char *cur_time, *prev_time;
+#endif
 
-#define MPU_UPDATE_RATE  (20)
+//=== LCD ===//
+#ifdef LCD_ON
+    int LCDOUT = 6;
+    int LCDIN = 3; //unused but required for SoftwareSerial
 
-//  MAG_UPDATE_RATE defines the rate (in Hz) at which the MPU updates the magnetometer data
-//  MAG_UPDATE_RATE should be less than or equal to the MPU_UPDATE_RATE
+    SoftwareSerial sLCD =  SoftwareSerial(LCDIN, LCDOUT);
+    
+    #define COMMAND 0xFE
+    #define CLEAR   0x01
+    #define LINE0   0x80
+    #define LINE1   0xC0
 
-#define MAG_UPDATE_RATE  (10)
+    void clear_lcd(void);    
+#endif
 
-//  MPU_MAG_MIX defines the influence that the magnetometer has on the yaw output.
-//  The magnetometer itself is quite noisy so some mixing with the gyro yaw can help
-//  significantly. Some example values are defined below:
+//=== Joystick ===//
+#ifdef JOYSTICK_ON
+    #define UP     A1
+    #define RIGHT  A2
+    #define DOWN   A3
+    #define CLICK  A4
+    #define LEFT   A5
+#endif
 
-#define  MPU_MAG_MIX_GYRO_ONLY          0                   // just use gyro yaw
-#define  MPU_MAG_MIX_MAG_ONLY           1                   // just use magnetometer and no gyro yaw
-#define  MPU_MAG_MIX_GYRO_AND_MAG       10                  // a good mix value 
-#define  MPU_MAG_MIX_GYRO_AND_SOME_MAG  50                  // mainly gyros with a bit of mag correction 
+//=== IMU ===//
+#ifdef IMU_ON
+    //  DEVICE_TO_USE selects whether the IMU at address 0x68 (default) or 0x69 is used
+    //    0 = use the device at 0x68
+    //    1 = use the device at ox69
 
-//  MPU_LPF_RATE is the low pas filter rate and can be between 5 and 188Hz
+    #define  DEVICE_TO_USE    0
 
-#define MPU_LPF_RATE   40
+    MPU9150Lib MPU;                             // the MPU object
 
-//  SERIAL_PORT_SPEED defines the speed to use for the debug serial port
+    //  MPU_UPDATE_RATE defines the rate (in Hz) at which the MPU updates the sensor data and DMP output
 
-#define  SERIAL_PORT_SPEED  9600
+    #define MPU_UPDATE_RATE  (20)
 
-void printQuaternion(File input, long *quat);
-void printQuaternion(File input, float *quat);
+    //  MAG_UPDATE_RATE defines the rate (in Hz) at which the MPU updates the magnetometer data
+    //  MAG_UPDATE_RATE should be less than or equal to the MPU_UPDATE_RATE
+
+    #define MAG_UPDATE_RATE  (10)
+
+    //  MPU_MAG_MIX defines the influence that the magnetometer has on the yaw output.
+    //  The magnetometer itself is quite noisy so some mixing with the gyro yaw can help
+    //  significantly. Some example values are defined below:
+
+    #define  MPU_MAG_MIX_GYRO_ONLY          0   // just use gyro yaw
+    #define  MPU_MAG_MIX_MAG_ONLY           1   // just use magnetometer and no gyro yaw
+    #define  MPU_MAG_MIX_GYRO_AND_MAG       10  // a good mix value 
+    #define  MPU_MAG_MIX_GYRO_AND_SOME_MAG  50  // mainly gyros with a bit of mag correction 
+
+    //  MPU_LPF_RATE is the low pas filter rate and can be between 5 and 188Hz
+
+    #define MPU_LPF_RATE   40
+
+    //  SERIAL_PORT_SPEED defines the speed to use for the debug serial port
+
+    #define  SERIAL_PORT_SPEED  9600
+
+    // print quarternion to files
+    void printQuaternion(File input, long *quat);
+    void printQuaternion(File input, float *quat);
+#endif
+
+//=== SETUP ==================================================================//
 
 void setup()
 {
+  // Serial debugging port 
   Serial.begin(9600);
+  Wire.begin();
+  
   //=============RTC===============//
   //clear out all the registers
   rtc.initClock();
@@ -97,6 +166,10 @@ void setup()
   rtc.setDate(14, 6, 3, 20, 10);
   //hr, min, sec
   rtc.setTime(1, 15, 40);
+  
+  //==
+  MPU.selectDevice(DEVICE_TO_USE);                        // only really necessary if using device 1
+  MPU.init(MPU_UPDATE_RATE, MPU_MAG_MIX_GYRO_AND_MAG, MAG_UPDATE_RATE, MPU_LPF_RATE);   // start the MPU
   
   //=============SD CARD===============//
   Serial.print("Initializing SD card...");
@@ -120,21 +193,23 @@ void setup()
     while (1) ;
   }
   
+  pinMode(LCDOUT, OUTPUT);
+  
   pinMode(LED2, OUTPUT); 
   pinMode(LED3, OUTPUT); 
  
   digitalWrite(LED2, LOW);
   pinMode(UP,INPUT);
   pinMode(DOWN,INPUT);
-  pinMode(LEFT,INPUT);
+  //pinMode(LEFT,INPUT);
   pinMode(RIGHT,INPUT);
-  pinMode(CLICK,INPUT);
+  //pinMode(CLICK,INPUT);
   
   digitalWrite(UP, HIGH);       /* Enable internal pull-ups */
   digitalWrite(DOWN, HIGH);
-  digitalWrite(LEFT, HIGH);
+  //digitalWrite(LEFT, HIGH);
   digitalWrite(RIGHT, HIGH);
-  digitalWrite(CLICK, HIGH);
+  //digitalWrite(CLICK, HIGH);
   
   sLCD.begin(9600);              /* Setup serial LCD and clear the screen */
   clear_lcd();
@@ -142,7 +217,9 @@ void setup()
   
   delay(1500);
 }
+//============================================================================//
 
+//=== Main Loop ==============================================================//
 void loop()
 {
   
@@ -164,19 +241,19 @@ void loop()
     sLCD.print(LINE0);
     sLCD.print(cur_time);
     
-//    MPU.selectDevice(DEVICE_TO_USE);                         // only needed if device has changed since init but good form anyway
-//    if (MPU.read()) {                                        // get the latest data if ready yet
+    MPU.selectDevice(DEVICE_TO_USE);                         // only needed if device has changed since init but good form anyway
+    if (MPU.read()) {                                        // get the latest data if ready yet
   //  MPU.printQuaternion(MPU.m_rawQuaternion);              // print the raw quaternion from the dmp
   //  MPU.printVector(MPU.m_rawMag);                         // print the raw mag data
   //  MPU.printVector(MPU.m_rawAccel);                       // print the raw accel data
   //  MPU.printAngles(MPU.m_dmpEulerPose);                   // the Euler angles from the dmp quaternion
   //  MPU.printVector(MPU.m_calAccel);                       // print the calibrated accel data
   //  MPU.printVector(MPU.m_calMag);                         // print the calibrated mag data
-//      MPU.printAngles(MPU.m_fusedEulerPose);                 // print the output of the data fusion
+      MPU.printAngles(MPU.m_fusedEulerPose);                 // print the output of the data fusion
       
-//      printQuaternion(dataFile,MPU.m_fusedEulerPose);
+      printQuaternion(dataFile,MPU.m_fusedEulerPose);
       
-//    }
+    }
     Serial.println();
     dataFile.println();
 
@@ -189,7 +266,7 @@ void loop()
   //Serial.println(count);
   
   count++;
-  if (digitalRead(CLICK) == 0){
+  if (digitalRead(UP) == 0){
     Serial.println("Stopping functionality");
     clear_lcd();
     
@@ -203,25 +280,33 @@ void loop()
 //  delay(1000);
 
 }
+//============================================================================//
 
-void clear_lcd(void)
-{
-  sLCD.write(COMMAND);
-  sLCD.write(CLEAR);
-}  
+//== Aux functions ===========================================================//
 
-void printQuaternion(File input, long *quat)
-{
-  input.print("w: "); input.print(quat[QUAT_W]);  
-  input.print(" x: "); input.print(quat[QUAT_X]);  
-  input.print(" y: "); input.print(quat[QUAT_Y]);  
-  input.print(" z: "); input.print(quat[QUAT_Z]);  
-}
+#ifdef LCD_ON
+    void clear_lcd(void)
+    {
+      sLCD.write(COMMAND);
+      sLCD.write(CLEAR);
+    }
+#endif
 
-void printQuaternion(File input, float *quat)
-{
-  input.print("w: "); input.print(quat[QUAT_W]);  
-  input.print(" x: "); input.print(quat[QUAT_X]);  
-  input.print(" y: "); input.print(quat[QUAT_Y]);  
-  input.print(" z: "); input.print(quat[QUAT_Z]);  
-}
+#ifdef IMU
+    void printQuaternion(File input, long *quat)
+    {
+      input.print("w: "); input.print(quat[QUAT_W]);  
+      input.print(" x: "); input.print(quat[QUAT_X]);  
+      input.print(" y: "); input.print(quat[QUAT_Y]);  
+      input.print(" z: "); input.print(quat[QUAT_Z]);  
+    }
+
+    void printQuaternion(File input, float *quat)
+    {
+      input.print("w: "); input.print(quat[QUAT_W]);  
+      input.print(" x: "); input.print(quat[QUAT_X]);  
+      input.print(" y: "); input.print(quat[QUAT_Y]);  
+      input.print(" z: "); input.print(quat[QUAT_Z]);  
+    }
+#endif 
+//============================================================================//
